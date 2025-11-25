9fa875a8895a88b2d67cb6ecd616d5081545dc91
aschweid <aschweid@umich.edu>
2025-11-25 14:52:45 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,11 +2,49 @@ document.getElementById("year").innerHTML = new Date().getFullYear();
 
 //https://www.w3schools.com/howto/howto_css_overlay.asp
 
-function myFunction() {
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('button.menu_icon').forEach(function (title) {
+        title.addEventListener('click', function () {
+            title.parentElement.classList.toggle('active');
+        });
+    });
+});
+
+
+document.querySelector(".menu_icon").addEventListener("resize", function () {
+    if (screen.width > 820) {
+        console.log(screen.width);
+        document.getElementsByClassName("menu_icon").tabIndex = -1;
+    }
+});
+
+
+document.querySelector(".menu_icon").addEventListener("click", function () {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
         x.style.display = "none";
     } else {
         x.style.display = "block";
     }
-}
+});
+
+
+document.querySelector(".menu_icon").addEventListener("keydown", function (event) {
+    if (event.key == "Enter") {
+        var x = document.getElementById("myLinks");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    }
+});
+
+
+
+document.querySelector(".recipe_of_the_week_card").addEventListener("keydown", function (event) {
+    if (event.key == "Enter") {
+        document.querySelectorAll(".recipe_of_the_week_card").click();
+    }
+});
